bbf2d2d8bca00204a00834b1cf2b7628088fe02d
chiranjsutariya372 <chiranjsutariya372@gmail.com>
2023-11-27 17:48:32 +0530

    PROJECT-TODO-APP

--- controller/controllers.js
+++ controller/controllers.js
@@ -8,12 +8,11 @@ const register  = (req,res)=>{
     res.render('singup')
 }
 const registerpost = async(req, res)=>{
-    const {username,email,password} = req.body
-    const security= await bcrypt.hash(password,10)
+    const {username,email,password}=req.body
+    let security =await bcrypt.hash(password,10)
     let data=await user.create({username,email,password:security})
     console.log(data);
-    res.cookie('token',username)
-    res.redirect('/home')
+    res.cookie("token",username).redirect("/login")
 }
 const login=(re,res)=>{
     res.render('login')
@@ -22,17 +21,15 @@ const loginpost = async(req, res)=>{
     const {email,password}=req.body
     let find=await user.findOne({email:email})
     if(!find)
-        {return res.send("user not found")}
+    {return res.send("email and password not match")}
 
-        let pass=await bcrypt.compare(password,find.password)
-    
-    if(find.email !== email || !pass)
-    {return res.send("email and password mismatch")}
+    let pass=await bcrypt.compare(password,find.password)
+    if(find.email != email || !pass)
+    {return res.send("email and password not match")}
 
-    let token = jwt.sign({id:find.id},"kjgiuregfdlkgrtui")
+    let token=jwt.sign({id:find.id},"kdshfdiufyerifcdkfh")
     console.log(token);
 
-    res.cookie('token',token).redirect('/todo')
-
+    res.cookie("token",token).redirect("/home")
 }
 module.exports={home,register,registerpost,login,loginpost}
